e470fd0ceeb313a25aa6a27a1e1a0484e2088858
raoyashwant <raoyashwant132@gmail.com>
2023-08-18 22:42:22 +0530

    Font changed in about and resume page

--- src/pages/about.js
+++ src/pages/about.js
@@ -20,10 +20,10 @@ function About() {
         </p>
       </div>
       <div>
-        <p className='det'>Student at Veltech University</p>
+        <p className='det'>Student and a self taught programmer.</p>
       </div>
       <div className='skill'>
-        <p><span> Web </span> /<span> App </span>/<span> Game Dev </span>/<span> 3D Modelling </span></p>
+        <p><span> Web </span> /<span> App </span>/<span> Game Dev </span>/<span> Animations </span></p>
       </div>
       <div  className='logos'>
         <Link to='https://github.com/Y-A-S-H-W-A-N-T' className='logo github'><img src={Github} alt='github'/></Link>
