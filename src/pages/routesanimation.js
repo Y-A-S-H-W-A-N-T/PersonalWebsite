b2279039dfb142937d4a0dc92302f4fab92bdfe4
raoyashwant <raoyashwant132@gmail.com>
2023-08-25 00:38:06 +0530

    making the website responsive

--- src/pages/routesanimation.js
+++ src/pages/routesanimation.js
@@ -1,11 +1,10 @@
 import React from 'react'
-import Contact from './contact'
 import Resume from './resume'
 import About from './about'
 import Projects from './projects'
 import { Route, Routes, useLocation } from 'react-router-dom'
 import { AnimatePresence } from "framer-motion"
-import Footer from './footer'
+// import Footer from './footer'
 
 
 function Routesanimation() {
@@ -16,11 +15,10 @@ function Routesanimation() {
     <AnimatePresence>
         <Routes location={location} key={location.pathname}>
           <Route exact path='/' element={<About/>}/>
-          <Route exact path='/contact' element={<Contact/>}/>
           <Route exact path='/resume' element={<Resume/>}/>
           <Route exact path='/projects' element={<Projects/>}/>
         </Routes>
-        <Footer/>
+        {/* <Footer/> */}
     </AnimatePresence>
   )
 }
